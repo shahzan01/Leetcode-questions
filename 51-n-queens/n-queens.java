4f5d125054b0ae114cad01c8dbd535707d76881e
Mohd shahzan <m.shahzan2003@gmail.com>
2024-03-02 11:17:26 +0530

    Time: 11 ms (12.92%) | Memory: 45 MB (29.87%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -56,22 +56,7 @@ board[i][col]='.';
 
 
 
-public static boolean isSafe(char board[][],int row,int col){
 
-//upper row
-for(int i=row-1;i>=0;i--){
-if(board[i][col]=='Q'){return false;}
-}
-//left diagonal
-for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-  if(board[i][j]=='Q'){return false;}
-}
-//right diagonal
-for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
-  if(board[i][j]=='Q'){return false;}
- 
-} return true;
-}
 
 
 
