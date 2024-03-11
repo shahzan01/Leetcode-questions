f66f068bf75553d0d684c04e42edf555d9322eb2
Mohd shahzan <m.shahzan2003@gmail.com>
2024-03-11 14:43:37 +0530

    Time: 4 ms (24.18%) | Memory: 41.5 MB (62.59%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,35 +1,31 @@
 class Solution {
     public String customSortString(String order, String s) {
-          int prio[] = new int[26];
-        int n = order.length();
-        for (int i = 0; i < order.length(); i++) {
-            char c = order.charAt(i);
-            prio[c - 'a'] = n;
-            n--;
-        }
- PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> prio[b - 'a'] - prio[a - 'a']);
-        HashMap<Character, Integer> hm = new HashMap<>();
-       for (int i = 0; i < s.length(); i++) {
+          HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             hm.put(c, hm.getOrDefault(c, 0) + 1);
-            if (!pq.contains(c)) {
-                pq.add(c);
-            }
         }
-       
-       
-        String ans = "";
+        System.out.println(hm);
         StringBuilder sb = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-            char c = pq.poll();
-            int freq = hm.get(c);
-             
-            while (freq > 0) {
-                sb.append(c);
-                freq--;
+        for (int i = 0; i < order.length(); i++) {
+            char c = order.charAt(i);
+            if (hm.containsKey(c)) {
+                int fre = hm.get(c);
+                while (fre > 0) {
+                    sb.append(c);
+                    fre--;
+                }
+                hm.remove(c);
             }
         }
-        return sb.toString();  
+
+        for (Character cur : hm.keySet()) {
+            int fre = hm.get(cur);
+            while (fre > 0) {
+                sb.append(cur);
+                fre--;
+            }
+        }
+        return sb.toString();
     }
 }
