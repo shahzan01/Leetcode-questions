8cbf3ef100d9a83ceb61d3bd5cbb75183f56f887
Mohd shahzan <m.shahzan2003@gmail.com>
2024-03-11 14:34:57 +0530

    Time: 3 ms (36.64%) | Memory: 41.9 MB (15.58%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -7,20 +7,17 @@ class Solution {
             prio[c - 'a'] = n;
             n--;
         }
-
+ PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> prio[b - 'a'] - prio[a - 'a']);
         HashMap<Character, Integer> hm = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+       for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             hm.put(c, hm.getOrDefault(c, 0) + 1);
-
-        }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> prio[b - 'a'] - prio[a - 'a']);
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-          if (!pq.contains(c)) {
+            if (!pq.contains(c)) {
                 pq.add(c);
             }
         }
+       
+       
         String ans = "";
         StringBuilder sb = new StringBuilder();
 
