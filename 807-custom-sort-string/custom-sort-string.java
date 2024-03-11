1d72a1c7d5c5106b5be7ec5e90d3292d520baa58
Mohd shahzan <m.shahzan2003@gmail.com>
2024-03-11 14:32:53 +0530

    Time: 4 ms (24.18%) | Memory: 41.3 MB (85.81%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String customSortString(String order, String s) {
+          int prio[] = new int[26];
+        int n = order.length();
+        for (int i = 0; i < order.length(); i++) {
+            char c = order.charAt(i);
+            prio[c - 'a'] = n;
+            n--;
+        }
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+
+        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> prio[b - 'a'] - prio[a - 'a']);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+          if (!pq.contains(c)) {
+                pq.add(c);
+            }
+        }
+        String ans = "";
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            char c = pq.poll();
+            int freq = hm.get(c);
+             
+            while (freq > 0) {
+                sb.append(c);
+                freq--;
+            }
+        }
+        return sb.toString();  
+    }
+}
