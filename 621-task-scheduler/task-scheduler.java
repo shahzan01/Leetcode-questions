80ebaafc390ffe15b67e180e1aaddda75179a8de
Mohd shahzan <m.shahzan2003@gmail.com>
2024-03-19 20:28:01 +0530

    Time: 2 ms (99.12%) | Memory: 45.7 MB (46.90%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,25 +1,23 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         HashMap<Character, Integer> hm = new HashMap<>();
-        int max = Integer.MIN_VALUE;
-        char maxc = 'A';
+        
+
+             int freq[] = new int[26];
         for (int i = 0; i < tasks.length; i++) {
             char c = tasks[i];
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
-            if (max < hm.get(c)) {
-                max = hm.get(c);
-                maxc = c;
-            }
-
+            freq[c - 'A']++;
         }
-        int blank = (max - 1) * n;
 
-        for (char c : hm.keySet()) {
-            if (c != maxc) {
-                blank -= Math.min(hm.get(c), max - 1);
-            }
-        }
-         return tasks.length+Math.max(0, blank);
+        Arrays.sort(freq);
+    
+        int max=freq[25];
+        int blank=(max-1)*n;
+for(int i=freq.length-2;i>=0;i--){
+if(freq[i]==0){break;}
+blank-=Math.min(freq[i], max-1);
+
+}
+return tasks.length+Math.max(blank, 0);
     }
 
 }
