01c75b46a198d6e667edf735429e04a37aaec8fc
Mohd shahzan <m.shahzan2003@gmail.com>
2024-04-13 13:39:50 +0530

    Time: 87 ms (41.43%) | Memory: 57.4 MB (51.12%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,37 +1,22 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
-         int n = arr.length;
-        int ls[] = new int[n];
-        int rs[] = new int[n];
-
+           int n = arr.length;
+    
         Stack<Integer> st = new Stack<>();
         st.push(-1);
-        for (int i = 0; i < n; i++) {
-            int val = arr[i];
+        int maxA = 0;
+        for (int i = 0; i <= n; i++) {
+            int val = i == n ? -1 : arr[i];
 
-            while (st.peek() != -1 && arr[st.peek()] >= val) {
+            while (st.peek() != -1 && arr[st.peek()] > val) {
+                int h = arr[st.peek()];
                 st.pop();
+
+                maxA = Math.max(maxA, (i - st.peek() - 1) * h);
             }
-            ls[i] = st.peek();
             st.push(i);
 
-        }
-        Stack<Integer> s = new Stack<>();
-        s.push(-1);
-        for (int i = n - 1; i >= 0; i--) {
-            int val = arr[i];
-            while (s.peek() != -1 && arr[s.peek()] >= val) {
-                s.pop();
-            }
-            rs[i] = s.peek() == -1 ? n : s.peek();
-            s.push(i);
-
-        }
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int ar = (rs[i] - ls[i] - 1) * arr[i];
-            max = Math.max(max, ar);
-        } 
-        return max;
+       }
+       return maxA;
     }
 }
