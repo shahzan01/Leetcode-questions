c1ca24ab977386b5b8a9a9df3720a98287f444d9
Mohd shahzan <m.shahzan2003@gmail.com>
2024-04-29 07:48:17 +0530

    Time: 1 ms (100.00%) | Memory: 55 MB (83.42%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,39 +1,18 @@
 class Solution {
     public int minOperations(int[] arr, int k) {
-         String bin = Integer.toBinaryString(k);
-        int onesCount[] = new int[35];
-
+ 
+        int xor = 0;
         for (int i = 0; i < arr.length; i++) {
-            String s = Integer.toBinaryString(arr[i]);
-            int idx = 0;
-            for (int j = s.length() - 1; j >= 0; j--) {
-                if (s.charAt(j) == '1') {
-                    onesCount[idx]++;
-                }
-                idx++;
-            }
-
-        }
-        HashSet<Integer> hs = new HashSet<>();
-        int idx = 0;
-        for (int i = bin.length() - 1; i >= 0; i--) {
-            if (bin.charAt(i) == '1') {
-                hs.add(idx);
-            }
-            idx++;
+            xor = xor ^ arr[i];
         }
         int ans = 0;
-
-        for (int i = 0; i < onesCount.length; i++) {
-            if (hs.contains(i)) {
-                if (onesCount[i] % 2 == 0) {
-                    ans++;
-                }
-            } else {
-                if (onesCount[i] % 2 != 0) {
-                    ans++;
-                }
+        while (k > 0 || xor > 0) {
+            if (k % 2 != xor % 2) {
+                ans++;
             }
-        }return ans;
+            k /= 2;
+            xor /= 2;
+        }
+         return ans;
     }
 }
