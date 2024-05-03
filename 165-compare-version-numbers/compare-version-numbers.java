46ea1cbd35353e5951ad84335c0fea72cbead244
Mohd shahzan <m.shahzan2003@gmail.com>
2024-05-03 18:53:41 +0530

    Time: 1 ms (78.87%) | Memory: 41.1 MB (83.58%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,71 +1,16 @@
 class Solution {
     public int compareVersion(String v1, String v2) {
-       int ve1[] = new int[500];
-        int ve2[] = new int[500];
+            String ve1[] = v1.split("\\.");
+        String ve2[] = v2.split("\\.");
 
-        int n = v1.length();
-        int m = v2.length();
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int l = 0;
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-
-        while (i < n && j < m) {
-            if (v1.charAt(i) == '.') {
-                ve1[k] = Integer.parseInt(sb1.toString());
-                i++;
-                k++;
-                sb1.setLength(0);
-            } else {
-                sb1.append(v1.charAt(i));
-                i++;
-
-            }
-            if (v2.charAt(j) == '.') {
-                ve2[l] = Integer.parseInt(sb2.toString());
-                j++;
-                l++;
-                sb2.setLength(0);
-            } else {
-                sb2.append(v2.charAt(j));
-                j++;
-            }
-
-        }
-        while (i < n) {
-            if (v1.charAt(i) == '.') {
-                ve1[k] = Integer.parseInt(sb1.toString());
-                i++;
-                k++;
-                sb1.setLength(0);
-            } else {
-                sb1.append(v1.charAt(i));
-                i++;
-
-            }
-
-        }
-        while (j < m) {
-            if (v2.charAt(j) == '.') {
-                ve2[l] = Integer.parseInt(sb2.toString());
-                j++;
-                l++;
-                sb2.setLength(0);
-            } else {
-                sb2.append(v2.charAt(j));
-                j++;
-            }
-        }
-        ve2[l] = Integer.parseInt(sb2.toString());
-        ve1[k] = Integer.parseInt(sb1.toString());
-  int   ans=0; 
-for( i=0;i<ve1.length;i++){
-if(ve1[i]>ve2[i]){ans=1;break;}
-if(ve1[i]<ve2[i]){ans=-1;break;}
-}
-return ans;
-  
+     int idx=Math.max(ve1.length, ve2.length);
+     for(int i=0;i<idx;i++){
+       int n1=i<ve1.length?Integer.parseInt(ve1[i]):0;
+        int n2=i<ve2.length?Integer.parseInt(ve2[i]):0;
+        if(n1>n2){return    1;}
+if(n1<n2){return -1;}
+     }
+       
+return 0;
     }
 }
