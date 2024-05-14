aad53eb7a1374cf8b5b7f3afd871b90150bd2e1f
Mohd shahzan <m.shahzan2003@gmail.com>
2024-05-14 12:29:52 +0530

    Time: 67 ms (63.07%) | Memory: 41.3 MB (35.87%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,18 +1,19 @@
 class Solution {
-     static int maxgold(int grid[][], int i, int j, boolean visit[][]) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0 || visit[i][j] == true) {
+    
+    static int maxgold(int grid[][], int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
             return 0;
         }
 
         int cur = grid[i][j];
-        visit[i][j] = true;
-        int left = maxgold(grid, i, j + 1, visit);
-        int right = maxgold(grid, i, j - 1, visit);
-        int up = maxgold(grid, i - 1, j, visit);
-        int down = maxgold(grid, i + 1, j, visit);
+        grid[i][j] = 0;
+        int left = maxgold(grid, i, j + 1);
+        int right = maxgold(grid, i, j - 1);
+        int up = maxgold(grid, i - 1, j);
+        int down = maxgold(grid, i + 1, j);
 
         int max = Math.max(Math.max(up, down), Math.max(left, right));
-        visit[i][j] = false;
+        grid[i][j] = cur;
         return cur + max;
 
     }
@@ -20,12 +21,12 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        boolean visit[][] = new boolean[m][n];
+    
         int ans = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] != 0) {
-                    int gold = maxgold(grid, i, j, visit);
+                    int gold = maxgold(grid, i, j);
                     ans = Math.max(ans, gold);
                 }
             }
