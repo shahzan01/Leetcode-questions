3c1b5a1e0f2d52e976861008796176ddd4f4cdbf
Mohd shahzan <m.shahzan2003@gmail.com>
2024-05-27 11:53:32 +0530

    Time: 2 ms (47.70%) | Memory: 40.9 MB (83.20%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,11 +1,13 @@
 class Solution {
-       static int binarySearch(int arr[], int k) {
-           if (arr.length == 1) {
-            if (k >= arr[0]) {
-                return -1;
+  
+    static int binarySearch(int arr[], int k) {
+        if (arr.length == 1) {
+            if (k > arr[0]) {
+                return 0;
             }
-            return 0;
+            return 1;
         }
+        int n = arr.length;
         int low = 0;
         int high = arr.length - 1;
         while (low != high) {
@@ -16,21 +18,35 @@ class Solution {
                 low = mid + 1;
             }
         }
-        if (low == arr.length - 1 && low != 0) {
-            return (arr[low] == k) ? low : -1;
+        if (low == arr.length-1) {
+
+            return (arr[low] == k) ? n - low : 0;
         }
-        return low;
+        return n - low;
 
     }
     public int specialArray(int[] arr) {
        int n=arr.length;
-        Arrays.sort(arr);
+        if(arr.length==2 && arr[0]==arr[1] && arr[0]==0){return -1;}
     
-        for (int i = 0; i <= 100; i++) {
-       int idx=binarySearch(arr, i);
-       if(idx==-1){return -1;}
-       if(n-idx==i){return i;}
-       
+     Arrays.sort(arr);
+
+        int low = 0;
+        int high = 100;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int num = binarySearch(arr, mid);
+            if (num == mid) {
+                return mid;
+            }
+
+            if (num > mid) {
+                low = mid + 1;
+            } else {
+                high = mid -1;
+            }
+
         }
 return -1;
 
