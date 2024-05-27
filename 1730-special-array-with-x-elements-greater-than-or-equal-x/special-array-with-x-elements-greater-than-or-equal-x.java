c234bbc5f7e993a9fc54a5f32211a059e60b7d8e
Mohd shahzan <m.shahzan2003@gmail.com>
2024-05-27 11:31:38 +0530

    Time: 2 ms (47.70%) | Memory: 41.2 MB (38.48%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,15 +1,38 @@
 class Solution {
+       static int binarySearch(int arr[], int k) {
+           if (arr.length == 1) {
+            if (k >= arr[0]) {
+                return -1;
+            }
+            return 0;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low != high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (low == arr.length - 1 && low != 0) {
+            return (arr[low] == k) ? low : -1;
+        }
+        return low;
+
+    }
     public int specialArray(int[] arr) {
-        for(int i=0;i<=100;i++){
-int count=0;
-for(int j=0;j<arr.length;j++){
-    if(arr[j]>=i){count++;}
-}
-if(count==i){return i;}
-}
+       int n=arr.length;
+        Arrays.sort(arr);
+    
+        for (int i = 0; i <= 100; i++) {
+       int idx=binarySearch(arr, i);
+       if(idx==-1){return -1;}
+       if(n-idx==i){return i;}
+       
+        }
 return -1;
-
-
 
     }
 }
