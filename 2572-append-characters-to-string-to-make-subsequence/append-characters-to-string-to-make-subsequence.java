800d110b1d58ae51379f9132a2f2c9e6be4b8e12
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-03 18:10:13 +0530

    Time: 7 ms (51.85%) | Memory: 45.3 MB (50.79%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
@@ -2,9 +2,8 @@ class Solution {
     public int appendCharacters(String s, String t) {
         int ans = 0, i = 0, j = 0;
         while (i < s.length() && j < t.length()) {
-            char a = s.charAt(i);
-            char b = t.charAt(j);
-            if (a == b) {
+           
+            if (s.charAt(i) == t.charAt(j)) {
                 j++;
             }
 
