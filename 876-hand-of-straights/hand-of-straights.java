9eb4014e6f2068fabbf2343bdcedf3f1d829e90a
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-06 10:52:15 +0530

    Time: 67 ms (12.22%) | Memory: 46.1 MB (8.75%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,32 @@
+class Solution {
+     static boolean fxn(int arr[], int gs) {
+        if (arr.length % gs != 0) {
+            return false;
+        }
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            tm.put(arr[i], tm.getOrDefault(arr[i], 0) + 1);
+        }
+        for (Integer i : tm.keySet()) {
+            int freq = tm.get(i);
+            if (freq == 0) {
+                continue;
+            }
+            while (freq-- > 0) {
+                for (int cur = i; cur < i + gs; cur++) {
+                      if (tm.getOrDefault(cur, 0) == 0) {
+                        return false;
+                    }
+                    tm.put(cur, tm.get(cur) - 1);
+                }
+            }
+
+        }
+
+        return true;
+
+    }
+    public boolean isNStraightHand(int[] arr, int gs) {
+        return fxn(arr,gs);
+    }
+}
