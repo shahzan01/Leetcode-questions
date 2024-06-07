03c1d8b81c12eca92557d135c033f0d7a9b49f74
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-07 10:54:17 +0530

    Time: 757 ms (5.05%) | Memory: 54.5 MB (86.38%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,28 +1,33 @@
 class Solution {
     public String replaceWords(List<String> dic, String sen) {
-          HashSet<String> hs = new HashSet<>();
+             HashSet<String> hs = new HashSet<>();
         for (String s : dic) {
             hs.add(s);
         }
         StringBuilder sb = new StringBuilder();
-         String arr[] = sen.split(" ");
-        l: for (int i = 0; i < arr.length; i++) {
-            String cur = arr[i];
-            StringBuilder temp = new StringBuilder();
-            for (int j = 0; j < cur.length(); j++) {
-                temp.append(cur.charAt(j));
-
-                if (hs.contains(temp.toString())) {
-                    arr[i] = temp.toString();
-                    sb.append(temp + " ");
-                    continue l;
-                }
-
+        StringBuilder temp = new StringBuilder();
+        l: for (int i = 0; i < sen.length(); i++) {
+            char c = sen.charAt(i);
+            if (c == ' ') {
+                System.out.println(temp);
+                sb.append(temp + " ");
+                temp.setLength(0);
+                continue;
             }
-            sb.append(arr[i] + " ");
+
+            temp.append(c);
+            if (hs.contains(temp.toString())) {
+                System.out.println();
+                while (i + 1 < sen.length() && sen.charAt(i + 1) != ' ') {
+                    i++;
+
+                }
+                continue l;
+            }
+
         }
-        sb.delete(sb.length() - 1, sb.length());
-       
-        return sb.toString();
+        sb.append(temp);
+   
+  return sb.toString();
     }
 }
