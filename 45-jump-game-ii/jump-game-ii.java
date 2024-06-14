c75c1f400abe999fbbbbf5b75e65c2ef696f276a
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-14 15:32:34 +0530

    Time: 31 ms (24.79%) | Memory: 45.3 MB (32.57%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,31 +1,21 @@
 class Solution {
     public int jump(int[] arr) {
-         int n = arr.length;
+        
+        int n = arr.length;
         int dp[] = new int[n];
-        dp[n - 1] = 0;
         for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] == 0) {
-                dp[i] = Integer.MAX_VALUE;
-                continue;
-            }
-            if (arr[i] + i >= n - 1) {
-                dp[i] = 1;
-                continue;
-            }
-            int val = arr[i];
+            int cur = arr[i];
             int min = Integer.MAX_VALUE;
-            while (val > 0) {
-                min = Math.min(min, dp[i + val]);
-                val--;
+            int jumpIdx = 1;
+            while (jumpIdx <= cur && jumpIdx+i < arr.length) {
+
+                min = Math.min(dp[i + jumpIdx], min);
+                jumpIdx++;
+
             }
-           
-            if (min == Integer.MAX_VALUE) {
-                dp[i] = Integer.MAX_VALUE;
-            } else {
-                dp[i] = min + 1;
-            }
+           dp[i] = min == Integer.MAX_VALUE ? min : min + 1;
 
         }
-        return dp[0];
+return dp[0];
     }
 }
