1dfb8b12704d2d5859cba8f0d38781ddc16a6e9b
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-18 19:52:43 +0530

    Time: 4 ms (8.97%) | Memory: 55.7 MB (34.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] arr) {
-         int n = arr.length;
-        int ans[] = new int[n];
-        ans[0] = 1;
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] * arr[i - 1];
+           int n = arr.length;
+        int left[] = new int[arr.length];
+        int right[] = new int[arr.length];
+        int idx = arr.length - 2;
+        left[0] = arr[0];
+        right[n - 1] = arr[n - 1];
+        for (int i = 1; i < arr.length; i++) {
+            left[i] = left[i - 1] * arr[i];
+            right[idx] = right[idx + 1] * arr[idx];
+            idx--;
 
         }
-        int right = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            ans[i] = ans[i] * right;
-            right = right * arr[i];
-        }
-        return ans;
+        for (int i = 0; i < arr.length; i++) {
+            int l = (i - 1 >= 0 ? left[i - 1] : 1);
+            int r = (i + 1 < n ? right[i + 1] : 1);
+            arr[i] = l * r;
+        }return arr;
     }
 }
