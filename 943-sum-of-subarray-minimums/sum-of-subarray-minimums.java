c54e7ced3f083adfbbbb0c3a15a0af74daa9b5d2
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-18 19:31:16 +0530

    Time: 62 ms (41.68%) | Memory: 50.9 MB (44.72%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,20 +1,10 @@
 class Solution {
-       static class pair {
-        int idx;
-        int val;
-
-        pair(int c, int p) {
-            this.idx = c;
-            this.val = p;
-        }
-
-    }
     public int sumSubarrayMins(int[] arr) {
          int n = arr.length;
         int next_smaller[] = new int[n];
         int prev_smaller[] = new int[n];
-        Stack<pair> st1 = new Stack<>();
-        Stack<pair> st2 = new Stack<>();
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
         for (int i = 0; i < n; i++) {
             prev_smaller[i] = i;
             next_smaller[i] = n - i - 1;
@@ -22,30 +12,28 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int cur = arr[i];
-            while (!st1.isEmpty() && cur < st1.peek().val) {
-                int j = st1.peek().idx;
+            while (!st1.isEmpty() && cur < arr[st1.peek()]) {
+                int j = st1.peek();
                 next_smaller[j] = i - j - 1;
 
                 st1.pop();
 
             }
 
-            st1.push(new pair(i, cur));
+            st1.push(i);
         }
         for (int i = n - 1; i >= 0; i--) {
             int cur = arr[i];
 
-            while (!st2.isEmpty() && cur <= st2.peek().val) {
-                int j = st2.peek().idx;
+            while (!st2.isEmpty() && cur <= arr[st2.peek()]) {
+                int j = st2.peek();
                 prev_smaller[j] = j - i - 1;
                 st2.pop();
             }
-            st2.push(new pair(i, cur));
-        }
-           int mod=1000000007; 
+            st2.push(i);
+        }           int mod=1000000007;
         long ans = 0;
-       
-        for (int i = 0; i < n; ++i) {
+         for (int i = 0; i < n; ++i) {
             ans += (long) ((next_smaller[i] + 1) * (prev_smaller[i] + 1)) % mod * arr[i] % mod;
             ans %= mod;
         }
