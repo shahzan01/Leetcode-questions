61c74cef546776eff4679c7cbb978a26e3e667db
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-18 19:24:48 +0530

    Time: 61 ms (43.27%) | Memory: 50.3 MB (54.08%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,44 +1,55 @@
 class Solution {
+       static class pair {
+        int idx;
+        int val;
+
+        pair(int c, int p) {
+            this.idx = c;
+            this.val = p;
+        }
+
+    }
     public int sumSubarrayMins(int[] arr) {
-        int length = arr.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-      
-        Arrays.fill(left, -1);
-        Arrays.fill(right, length);
-      
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+         int n = arr.length;
+        int next_smaller[] = new int[n];
+        int prev_smaller[] = new int[n];
+        Stack<pair> st1 = new Stack<>();
+        Stack<pair> st2 = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            prev_smaller[i] = i;
+            next_smaller[i] = n - i - 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int cur = arr[i];
+            while (!st1.isEmpty() && cur < st1.peek().val) {
+                int j = st1.peek().idx;
+                next_smaller[j] = i - j - 1;
+
+                st1.pop();
+
             }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
+
+            st1.push(new pair(i, cur));
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            int cur = arr[i];
+
+            while (!st2.isEmpty() && cur <= st2.peek().val) {
+                int j = st2.peek().idx;
+                prev_smaller[j] = j - i - 1;
+                st2.pop();
             }
-            stack.push(i);
+            st2.push(new pair(i, cur));
+        }
+           int mod=1000000007; 
+        long ans = 0;
+       
+        for (int i = 0; i < n; ++i) {
+            ans += (long) ((next_smaller[i] + 1) * (prev_smaller[i] + 1)) % mod * arr[i] % mod;
+            ans %= mod;
         }
       
-        stack.clear();
-      
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-      
-        int mod = (int) 1e9 + 7;
-        long answer = 0;
-      
-        for (int i = 0; i < length; ++i) {
-            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
-            answer %= mod;
-        }
-      
-        return (int) answer;
+        return (int) ans;
     }
 }
