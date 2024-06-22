a6fe2666f108663ba33a3100f06a1d9ad15e01a3
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-22 08:32:52 +0530

    Time: 49 ms (5.05%) | Memory: 54.2 MB (93.10%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,14 +1,11 @@
 class Solution {
     public int numberOfSubarrays(int[] arr, int k) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] % 2;
-        }
         int ans = 0;
         HashMap<Integer, Integer> freqofPrexSum = new HashMap<>();
         int sum = 0;
         freqofPrexSum.put(0, 1);
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+            sum += arr[i]%2==0?0:1;
             if (freqofPrexSum.containsKey(sum - k)) {
                 ans += freqofPrexSum.get(sum - k);
             }
