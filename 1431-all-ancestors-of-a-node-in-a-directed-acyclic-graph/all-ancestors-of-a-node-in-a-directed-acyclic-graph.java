57c344c6846923579c7e65259273e34f6ed98251
Mohd shahzan <m.shahzan2003@gmail.com>
2024-06-29 13:02:07 +0530

    Time: 200 ms (24.25%) | Memory: 100.8 MB (5.79%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,109 +1,47 @@
-class Solution { static List<Integer> merger(int parent, List<Integer> child) {
-        List<Integer> merged = new ArrayList<>();
-        int i = 0;
+class Solution { 
+     
 
-        while (i < child.size() && child.get(i) <= parent) {
-            merged.add(child.get(i));
-            i++;
+      public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        
+        // Build graph, and compute in degree.
+        int[] inDegree = new int[n];
+        Map<Integer, List<Integer>> parentToKids = new HashMap<>();
+        for (int[] e : edges) {
+            parentToKids.computeIfAbsent(e[0], l -> new ArrayList<>()).add(e[1]);
+            ++inDegree[e[1]];
         }
-        merged.add(parent);
-        i++;
-        while (i < child.size()) {
-            merged.add(child.get(i));
-        }
-        return merged;
-    }
-
-    static List<Integer> merger(List<Integer> parent, List<Integer> child) {
-        List<Integer> mergedArr = new ArrayList<>();
-        int m = 0;
-        int n = 0;
-        while (m < parent.size() && n < child.size()) {
-            if (parent.get(m) < child.get(n)) {
-                while (m + 1 < parent.size() && parent.get(m + 1) == parent.get(m)) {
-                    m++;
-                }
-                mergedArr.add(parent.get(m));
-                m++;
-            }
-
-            else if (parent.get(m) > child.get(n)) {
-                while (n + 1 < child.size() && child.get(n + 1) == child.get(n)) {
-                    n++;
-                }
-                mergedArr.add(child.get(n));
-                n++;
-            }
-
-            else {
-                while (m + 1 < parent.size() && parent.get(m + 1) == parent.get(m)) {
-                    m++;
-                }
-                while (n + 1 < child.size() && child.get(n + 1) == child.get(n)) {
-                    n++;
-                }
-                mergedArr.add(parent.get(m));
-                m++;
-                n++;
-            }
-
-        }
-        while (m < parent.size()) {
-            while (m + 1 < parent.size() && parent.get(m + 1) == parent.get(m)) {
-                m++;
-            }
-            mergedArr.add(parent.get(m));
-            m++;
-        }
-        while (n < child.size()) {
-            while (n + 1 < child.size() && child.get(n + 1) == child.get(n)) {
-                n++;
-            }
-            mergedArr.add(child.get(n));
-            n++;
-        }
-        return mergedArr;
-
-    }
-
-    public List<List<Integer>> getAncestors(int n, int[][] edges) {
-          int indegree[] = new int[n];
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            ans.add(new ArrayList<>());
-        }
-   
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            indegree[v]++;
-            map.computeIfAbsent(u, l -> new ArrayList<>()).add(v);
-
-        }
+        
+        // 1. Use a list of sets to save ancestors 
+        // and to avoid duplicates.
+        // 2. Use a Queue to save 0-in-degree nodes as
+        // the starting nodes for topological sort.
+        List<Set<Integer>> sets = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
+        for (int i = 0; i < n; ++i) {
+            sets.add(new HashSet<>());
+            if (inDegree[i] == 0)
+                q.offer(i);
+        }
+        
+        // BFS to their neighbors and decrease 
+        // the in degrees, when reaching 0, add
+        // it into queue;
+        // During this procedure, get direct parent, add 
+        // all ancestors of direct parents' of each kid.
+        while (!q.isEmpty()) {
+            int parent = q.poll();
+            for (int kid : parentToKids.getOrDefault(parent, Arrays.asList())) {
+                sets.get(kid).add(parent);
+                sets.get(kid).addAll(sets.get(parent));
+                if (--inDegree[kid] == 0)
+                    q.offer(kid);
             }
         }
-
-       while (!q.isEmpty()) {
-            int cur = q.poll();
-           
-            for (int neig : map.getOrDefault(cur, Arrays.asList())) {
-                List<Integer>t=new ArrayList<>();t.add(cur);
-                ans.set(neig, merger(ans.get(cur), ans.get(neig)));
-                ans.set(neig,merger(t, ans.get(neig)));
-                indegree[neig]--;
-                if (indegree[neig] == 0) {
-                    q.add(neig);
-                }
-
-            }
-
-        }
-     return ans;
+        
+        // Sort ancestors and put into return list. 
+        List<List<Integer>> ans = new ArrayList<>();
+        for (Set<Integer> set : sets)
+            ans.add(new ArrayList<>(new TreeSet<>(set)));
+        return ans;
     }
 }
