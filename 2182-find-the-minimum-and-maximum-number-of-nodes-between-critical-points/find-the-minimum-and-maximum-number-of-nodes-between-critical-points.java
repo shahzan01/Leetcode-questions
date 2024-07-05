92d968ce6f28d4a87b21dc1789903401e9a13ff6
Mohd shahzan <m.shahzan2003@gmail.com>
2024-07-05 18:05:21 +0530

    Time: 4 ms (100.00%) | Memory: 60.7 MB (44.14%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -12,34 +12,46 @@ class Solution {
     static int[] fxn(ListNode head){
 
 int ans[]={-1,-1};
-ArrayList<Integer>li=new ArrayList<>();
 ListNode curr=head.next;
 ListNode prev=head;
 int idx=2;
-
+int minlen=Integer.MAX_VALUE;
+int cpCount=0;
+int firstcp=-1;
+int lastcp=-1;
 while (curr.next!=null) {
-
 if(prev.val<curr.val && curr.val>curr.next.val){
-    li.add(idx);
+    cpCount++;
+    if(lastcp!=-1){
+        minlen=Math.min(minlen, idx-lastcp);
+    }
+    lastcp=idx;
 }
-
 else if(prev.val>curr.val && curr.next.val>curr.val){
-    li.add(idx);
+    cpCount++;
+    if(lastcp!=-1){
+        minlen=Math.min(minlen, idx-lastcp);
+    }
+    lastcp=idx;
 }
-
+if(cpCount==1 &&firstcp==-1){firstcp=idx;}
 idx++;
 prev=curr;
 curr=curr.next;
 }
 
-if(li.size()<=1){return ans;}
-int maxlen=li.get(li.size()-1)-li.get(0);
-int min=Integer.MAX_VALUE;
-for(int i=0;i<li.size()-1;i++){
-    min=Math.min(min, li.get(i+1)-li.get(i));
-}
-ans[0]=min;ans[1]=maxlen;
+if(cpCount<=1){return ans;}
+
+ans[0]=minlen;
+ans[1]=lastcp-firstcp;
+
+
+
+
+
 return ans;
+
+
 
 }
     public int[] nodesBetweenCriticalPoints(ListNode head) {
