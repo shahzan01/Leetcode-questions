55acc964a229a4a32dcefa6827923d564eb00207
Mohd shahzan <m.shahzan2003@gmail.com>
2024-07-19 10:04:02 +0530

    Time: 17 ms (5.52%) | Memory: 45.6 MB (6.00%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] mat) {
-            int m = mat.length;
+              int m = mat.length;
         int n = mat[0].length;
-        int row[] = new int[m];
-        int col[] = new int[n];
-        Arrays.fill(row, -1);
-        Arrays.fill(col, -1);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int rowmin = row[i] == -1 ? Integer.MAX_VALUE : mat[i][row[i]];
-                int colmax = col[j] == -1 ? Integer.MIN_VALUE : mat[col[j]][j];
-                if (rowmin > mat[i][j]) {
-                    row[i] = j;
-                }
-                if (colmax < mat[i][j]) {
-                    col[j] = i;
-                }
 
-            }
-        }
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < row.length; i++) {
-            int column = row[i];
-            if (col[column] == i) {
-                ans.add(mat[i][column]);
+        for (int i = 0; i < m; i++) {
+            int min = 0;
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] < mat[i][min]) {
+                    min = j;
+                }
+            }
+            int max = 0;
+            for (int j = 0; j < m; j++) {
+                if (mat[j][min] > mat[max][min]) {
+                    max = j;
+                }
+            }
+            System.out.println(min + "," + max);
+            if (max == i) {
+                ans.add(mat[i][min]);
             }
 
         }return ans;
