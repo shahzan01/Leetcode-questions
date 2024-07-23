576221ed29b35195d854f0635e6ff4755cea0da7
Mohd shahzan <m.shahzan2003@gmail.com>
2024-07-23 12:57:01 +0530

    Time: 9 ms (5.53%) | Memory: 42.3 MB (8.61%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+     HashMap<Character, String> map = new HashMap<>();
+        String arr[] = s.split(" ");
+        if(pattern.length() != arr.length){return false;}
+HashSet<String>hs=new HashSet<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            System.out.println(map);
+            if (map.containsKey(c) && !map.get(c).equals(arr[i])) {
+                return false;
+            } else if(!map.containsKey(c)){
+                if(hs.contains(arr[i])){return false;}
+                else{
+                    map.put(c, arr[i]);
+                    hs.add(arr[i]);
+                }
+            }
+            else{
+                continue;
+            }
+
+        }
+    return true;
+    }
+}
