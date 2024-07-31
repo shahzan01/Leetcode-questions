e08a4a77425d222c032253cf4e689c0ad36b1963
Mohd shahzan <m.shahzan2003@gmail.com>
2024-07-31 12:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (57.89%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+       static int f(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int dp[] = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] + dp[i + 2];
+
+        }
+  
+        return dp[0];
+
+    }
+    public int climbStairs(int n) {
+        return f(n);
+    }
+}
