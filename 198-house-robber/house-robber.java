4a030ddcdb474b6da006a0352010bb32bd6e28ec
Mohd shahzan <m.shahzan2003@gmail.com>
2024-07-31 12:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (68.67%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,16 @@
-public class Solution {
+class Solution {
+  static int hel(int arr[],int cur,boolean rob){
+ int dp[]=new int[arr.length+2];
+ dp[0]=0;dp[1]=0;
+ for(int i=2;i<dp.length;i++){
+    dp[i]=Math.max(dp[i-1], dp[i-2]+arr[i-2]);
+ }
+return dp[dp.length-1];
+
+
+ }
+
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return nums[0];
-        } else if (n == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; ++i) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
-        }
-
-        return dp[n-1];
+  return hel(nums,0,true);      
     }
 }
