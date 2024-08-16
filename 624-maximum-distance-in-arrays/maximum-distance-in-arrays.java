8fe87e6683f49e53d76784ee7eb741cc44b2dacb
Mohd shahzan <m.shahzan2003@gmail.com>
2024-08-16 09:59:23 +0530

    Time: 7 ms (82.70%) | Memory: 65 MB (21.52%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 1; i < arr.size(); i++) {
             int curSmallest = arr.get(i).get(0);
             int curLargest = arr.get(i).get(arr.get(i).size() - 1);
-              System.out.println(Math.abs(largest - curSmallest)+","+ Math.abs(curLargest - samllest));
+           
             max = Math.max(Math.abs(largest - curSmallest),max);
              max = Math.max(Math.abs(curLargest - samllest),max);
             largest = Math.max(largest, curLargest);
