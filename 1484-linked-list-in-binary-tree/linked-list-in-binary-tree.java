df032e14dfd1a7daa76a949f1614db071187b7e9
Mohd shahzan <m.shahzan2003@gmail.com>
2024-09-07 11:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (29.81%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -1,60 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-static boolean hel( List<Integer>li,TreeNode root,int idx ){
-
-    if(idx>=li.size()){return true;}
-    if(root==null){return false;}
-if(root.val!=li.get(idx)){return false;}
-return hel(li,root.left,idx+1) || hel(li,root.right,idx+1);
-
-}
-static boolean dfs(TreeNode root,int s,List<Integer>li){
-    if(root==null){return false;}
-if(root.val==s){if(hel(li,root,0)){return true;}}
-return dfs(root.left,s,li) || dfs(root.right,s,li);
-
-
-}
-
-
     public boolean isSubPath(ListNode head, TreeNode root) {
-        List<Integer>li=new ArrayList<>();
-        ListNode cur=head;
-        while(cur!=null){
-            li.add(cur.val);
-            cur=cur.next;
-        }
-        return dfs(root,li.get(0),li);
-// System.out.println(li);
-
-
-
-
-
-
+        return dfs(head, head, root);
     }
+
+    boolean dfs(ListNode head, ListNode cur, TreeNode root) {
+        if(cur == null) return true;
+        if(root == null) return false;
+        if(cur.val == root.val) cur = cur.next;
+        else if (head.val == root.val) head = head.next;
+        else cur = head;
+        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    }
+
 }
