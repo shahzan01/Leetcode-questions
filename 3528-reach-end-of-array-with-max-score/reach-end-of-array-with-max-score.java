3ca2ec095e19a6d9e2368d62488ad973a0347638
Mohd shahzan <m.shahzan2003@gmail.com>
2024-09-08 20:22:03 +0530

    Time: 2349 ms (100.00%) | Memory: 65.8 MB (100.00%) - LeetSync

---
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long findMaximumScore(List<Integer> A) {
+        long res = 0, ma = 0;
+        for (int a: A) {
+            res += ma;
+            ma = Math.max(ma, a);
+            
+        System.out.println(ma);
+
+        }
+        return res;
+    }
+}
