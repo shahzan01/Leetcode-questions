72097bb399be5652c26c12c972eb0bfe8ca18870
Mohd shahzan <m.shahzan2003@gmail.com>
2024-09-11 14:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (35.00%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,31 +1,10 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-         String s = Integer.toBinaryString(start);
-        String g = Integer.toBinaryString(goal);
-      
-
+         int xor = start ^ goal;
         int ans = 0;
-        int i = s.length() - 1;
-        int j = g.length() - 1;
-        while (i >= 0 && j >= 0) {
-            if (s.charAt(i) != g.charAt(j)) {
-                ans++;
-            }
-            i--;
-            j--;
-        }
-       
-        while (i >= 0) {
-            if (s.charAt(i) == '1') {
-                ans++;
-            }
-            i--;
-        }
-        while (j >= 0) {
-            if (g.charAt(j) == '1') {
-                ans++;
-            }
-            j--;
+        while (xor > 0) {
+            ans++;
+            xor = xor & (xor - 1);
         }
         return ans;
     }
