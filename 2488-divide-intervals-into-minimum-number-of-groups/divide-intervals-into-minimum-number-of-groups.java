95f505dcd901367e722ba8867381104d645beb00
Mohd shahzan <m.shahzan2003@gmail.com>
2024-10-12 11:29:02 +0530

    Time: 75 ms (70.92%) | Memory: 92.6 MB (21.74%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,33 @@
+class Solution {
+       static void dbg(Object... o) {
+        System.out.println(Arrays.deepToString(o));
+    }
+    public int minGroups(int[][] intervals) {
+        int n = intervals.length;
+        int[] start_times = new int[n];
+        int[] end_times = new int[n];
+
+        // Extract start and end times
+        for (int i = 0; i < n; i++) {
+            start_times[i] = intervals[i][0];
+            end_times[i] = intervals[i][1];
+        }
+
+        // Sort start and end times
+        Arrays.sort(start_times);
+        Arrays.sort(end_times);
+dbg(start_times,end_times);
+        int end_ptr = 0, group_count = 0;
+
+        // Traverse through the start times
+        for (int start : start_times) {
+            if (start > end_times[end_ptr]) {
+                end_ptr++;
+            } else {
+                group_count++;
+            }
+        }
+
+        return group_count;
+    }
+}
