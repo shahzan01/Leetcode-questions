40176d8771c72da34f569809ef0290a861fe2aaa
Mohd shahzan <m.shahzan2003@gmail.com>
2024-10-27 18:50:50 +0530

    Time: 15 ms (9.76%) | Memory: 54.7 MB (85.04%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -4,19 +4,16 @@ class Solution {
         int n=mat.length;
         int m=mat[0].length;
 int dp[][]=new int[n+1][m+1];
+HashMap<Integer,Integer>hm=new HashMap<>();
 for(int i=1;i<dp.length;i++){
     for(int j=1;j<dp[0].length;j++){
         if(mat[i-1][j-1]==0){continue;}
         int min=Math.min(Math.min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]);
         dp[i][j]=min+1;
+             hm.put(dp[i][j], hm.getOrDefault(dp[i][j],0)+1);
     }
 }
-HashMap<Integer,Integer>hm=new HashMap<>();
-for(int i=1;i<dp.length;i++){
-    for(int j=1;j<dp[0].length;j++){
-     hm.put(dp[i][j], hm.getOrDefault(dp[i][j],0)+1);
-    }
-}
+
 int ans=0;
 for(int i:hm.keySet()){
     ans+=i*hm.get(i);
