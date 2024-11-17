e15a34cdbfcc57edcade63c1c5db56f99373568c
Mohd shahzan <m.shahzan2003@gmail.com>
2024-11-17 14:24:26 +0530

    Time: 27 ms (92.59%) | Memory: 64 MB (5.75%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,40 +1,29 @@
-// sliding window + monotic stack(here dequeue)
-
-class Solution {
-    public int shortestSubarray(int[] nums, int k) {
-        int n = nums.length;
-        Deque<Pair> dq = new ArrayDeque<>(); // index, sum
-        long sum = 0;
-        int shortest = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; ++i) {
-            sum += nums[i];
-            if (sum >= k) {
-                shortest = Math.min(shortest, i + 1); // Sum from start to i-th index
-            }
-
-            // Reduce window size to find minimum window with sum >= k
-            while (!dq.isEmpty() && sum - dq.peekFirst().sum >= k) {
-                shortest = Math.min(shortest, i - dq.pollFirst().index);
-            }
-
-            // Maintain monotonically non-decreasing order of deque
-            while (!dq.isEmpty() && sum <= dq.peekLast().sum) {
-                dq.pollLast();
-            }
-            dq.addLast(new Pair(sum, i)); // Push i-th sum
+class Solution {  static class  pair {
+    long sum;int idx;
+        pair(int i,long s){
+            this.sum=s;
+            this.idx=i;
         }
-
-        return shortest == Integer.MAX_VALUE ? -1 : shortest;
     }
+    public int shortestSubarray(int[] arr, int k) {
+           Deque<pair>dq=new ArrayDeque<>();
+long sum=0;
+int ans=Integer.MAX_VALUE;
+for(int i=0;i<arr.length;i++){
+    sum+=arr[i];
+    if(sum>=k){
+        ans=Math.min(ans, i+1);
+    }
+while (!dq.isEmpty() && sum-dq.peekFirst().sum >=k) {
+    ans=Math.min(ans, i-dq.pollFirst().idx);
+}
+while (!dq.isEmpty() && sum<=dq.peekLast().sum) {
+    dq.pollLast();
+}
+dq.addLast(new pair(i, sum));
 
-    static class Pair {
-        long sum;
-        int index;
 
-        Pair(long sum, int index) {
-            this.sum = sum;
-            this.index = index;
-        }
+}
+return ans==Integer.MAX_VALUE?-1:ans;  
     }
 }
