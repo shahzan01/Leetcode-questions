ac155e480abc228bc600dfa4d6856e989e8202f2
Mohd shahzan <m.shahzan2003@gmail.com>
2024-12-01 09:26:15 +0530

    Time: 2 ms (93.52%) | Memory: 43.1 MB (26.42%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr.length;j++){
+            if(j==i){continue;}
+            if(arr[j]==arr[i]*2){return true;}
+        }
+    }    
+    return false;
+    }
+}
